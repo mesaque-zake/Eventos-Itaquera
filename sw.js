ddb3d5183562309384534393e15b3254090d13a5
Mesaque <imesalink@gmail.com>
2026-01-13 23:44:37 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
 
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Instalado');
+});
+
+self.addEventListener('fetch', (e) => {
+  // O iFrame precisa de rede, então apenas repassamos as requisições
+  e.respondWith(fetch(e.request));
+});
