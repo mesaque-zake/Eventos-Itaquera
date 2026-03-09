1c6ffca0c069f38e96e56142fe5e6195d555e273
Mesaque <imesalink@gmail.com>
2026-03-09 19:51:06 -0300

    Update cache name from 'acoes-v10' to 'acoes-v11'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'acoes-v10';
+const CACHE_NAME = 'acoes-v11';
 const assets = ['index.html', 'manifest.json', 'favicon.png', 'icone-192.png', 'icone-512.png', 'MesaLogo.png'];
 
 self.addEventListener('install', (e) => {
